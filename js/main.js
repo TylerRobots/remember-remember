3fc4b805f85f26a2409181fcb2c0cd3b911577d6
Tyler Robots <info@tylerrobotsdesign.com>
2019-10-27 16:19:22 -0500

    renamed statusPriority references to status, add logic to selected option for status dropdown menus, refactor removeRememberedThing()

--- js/main.js
+++ js/main.js
@@ -25,24 +25,24 @@ const LANG = { /* EN */
     'delete-confirm'            : "Please confirm.",
     'default-value-delete-confirm': "Delete",
 
-    'priority-n-3'              : "-3",
-    'priority-n-2'              : "-2",
-    'priority-n-1'              : "-1",
-    'priority-n01'              : "1",
-    'priority-n02'              : "2",
-    'priority-n03'              : "3",
-    'priority-n04'              : "4",
-    'priority-n05'              : "5",
+    'status-n-3'              : "-3",
+    'status-n-2'              : "-2",
+    'status-n-1'              : "-1",
+    'status-n01'              : "1",
+    'status-n02'              : "2",
+    'status-n03'              : "3",
+    'status-n04'              : "4",
+    'status-n05'              : "5",
 
-    'priority-w-3'              : "Removed",
-    'priority-w-2'              : "Archived",
-    'priority-w-1'              : "Completed",
-    'priority-w00'              : "No Priority",
-    'priority-w01'              : "Low",
-    'priority-w02'              : "Low-medium",
-    'priority-w03'              : "Medium",
-    'priority-w04'              : "Medium-high",
-    'priority-w05'              : "High",
+    'status-w-3'              : "Removed",
+    'status-w-2'              : "Archived",
+    'status-w-1'              : "Completed",
+    'status-w00'              : "No Priority",
+    'status-w01'              : "Low",
+    'status-w02'              : "Low-medium",
+    'status-w03'              : "Medium",
+    'status-w04'              : "Medium-high",
+    'status-w05'              : "High",
 
     'th-id'                     : "ID",
     'th-name'                   : "Name/Description",
@@ -141,9 +141,9 @@ function setInput( elementID, value ) {
  * @param {*} elementID Select an element by ID
  */
 function togglePopup( elementID ) {
-
+    
     document.getElementById( elementID ).classList.toggle( "hidden" );
-
+    
 }
 
 
@@ -154,38 +154,55 @@ function togglePopup( elementID ) {
  */
 
 
- /**
-  * 
-  */
+/**
+ * 
+ */
 function getLastID() {
-
+    
     let rememberedThings = getRememberedThings();
     let lastID = objectLength( rememberedThings );
-
+    
     return Number( lastID );
-
+    
 }
 
 
+/**
+ * 
+ */
 function getRememberedThings() {
-
+    
     let rememberedThings = JSON.parse( window.localStorage.getItem( "rememberedThings" ) );
-
+    
     return rememberedThings;
-
+    
 }
 
+
+/**
+ * 
+ * @param {*} thingID 
+ */
 function removeRememberedThing( thingID ) {
-
-    let thingElement = document.getElementById( "item-" + thingID );
-    thingElement.parentNode.removeChild( thingElement );
-
+    
     let rememberedThings = getRememberedThings();
     let selectedThing = rememberedThings[thingID];
-    rememberedThings[thingID].statusPriority = "-3";
-    window.localStorage.setItem( "rememberedThings", JSON.stringify( rememberedThings ) );
+    
+    removeTableRow( thingID );
 
+    rememberedThings[thingID].status = "-3";
+    window.localStorage.setItem( "rememberedThings", JSON.stringify( rememberedThings ) );
+    
     return rememberedThings;
+    
+}
+
+
+/**
+ * 
+ * @param {*} thingID 
+ */
+function updateRememberedThing( thingID ) {
 
 }
 
@@ -195,9 +212,9 @@ function removeRememberedThing( thingID ) {
  * @param {*} object 
  */
 function objectLength( object ) {
-
+    
     var length = 0;
-
+    
     for( var key in object ) {
         if( object.hasOwnProperty( key ) ) {
             ++length;
@@ -211,19 +228,27 @@ function objectLength( object ) {
 /**
  * 
  */
+function  clearAllTableRows() {
+
+}
+
+
+/**
+ * 
+ */
 function createTableRow( thing ) {
-
+    
     let elementID = "table-header";
-
+    
     let node = document.createElement( "div" );
-    node.setAttribute( "class", "remember__table__tr remember__table__tr--priority__" + thing.statusPriority );
+    node.setAttribute( "class", "remember__table__tr remember__table__tr--status__" + thing.status );
     node.setAttribute( "id", "item-" + thing.id );
     let nodePosition = document.getElementById( elementID ).parentElement;
     nodePosition.insertBefore(node, nodePosition[1]);
-
+                
     let tableRow = `
-        <div class="remember__table__td__priority">
-            ${thing.statusPriority}
+        <div class="remember__table__td__status">
+            ${thing.status}
         </div>
         <div class="remember__table__td__id">
             ${thing.id}
@@ -242,69 +267,105 @@ function createTableRow( thing ) {
             ${thing.dateModified}
         </div>
         <div class="remember__table__td__actions">
-        <span>
-            <select class="remember__table_td__actions__input__status-priority" id="status-priority-${thing.id}" name="statusPriority">
-                <option value="-1">Complete</option>
-                <option value="0">No Priority</option>
-                <option value="1">1 (low)</option>
-                <option value="2">2</option>
-                <option value="3">3 (medium)</option>
-                <option value="4">4</option>
-                <option value="5">5 (high)</option>
-            </select>
-        </span>
-
-        <span>
-            <label for="favorite-${thing.id}">
+            <span>
+                <select
+                  class="remember__table_td__actions__input__status"
+                  id="status-${thing.id}"
+                  name="status"
+                  onChange="updateRememberedThing('${thing.id}');" >
+                
+                    <option value="-3" disabled ${selectStatus(thing.status, -3 )}>
+                        Removed
+                    </option>
+                    <option value="-2" disabled ${selectStatus(thing.status, -2 )}>
+                        Archived
+                    </option>
+                    <option value="-1" ${selectStatus(thing.status, -1 )}>
+                        Completed
+                    </option>
+                    <option value="0" ${selectStatus(thing.status, 0 )}>
+                        No Priority
+                    </option>
+                    <option value="1" ${selectStatus(thing.status, 1 )}>
+                        1 (low)
+                    </option>
+                    <option value="2" ${selectStatus(thing.status, 2 )}>
+                        2
+                    </option>
+                    <option value="3" ${selectStatus(thing.status, 3 )}>
+                        3 (medium)
+                    </option>
+                    <option value="4" ${selectStatus(thing.status, 4 )}>
+                        4
+                    </option>
+                    <option value="5" ${selectStatus(thing.status, 5 )}>
+                        5 (high)
+                    </option>
+            
+                </select>
+            </span>
+            
+            <span>
+                <label for="favorite-${thing.id}">
                     Favorite
-            </label>
-            <input id="favorite-${thing.id}" name="favorite" type="checkbox" />
-        </span>
+                </label>
+                <input id="favorite-${thing.id}" name="favorite" type="checkbox" />
+            </span>
+            
+            <span>
+                <input
+                  id="delete-${thing.id}"
+                  type="button"
+                  value="Delete"
+                  onclick="togglePopup('dialog-box__confirm-delete-${thing.id}');"
+                />
 
-        <span>
-            <input
-            id="delete-${thing.id}"
-            type="button"
-            value="Delete"
-            onclick="togglePopup('dialog-box__confirm-delete-${thing.id}');"
-            />
-            <div id="dialog-box__confirm-delete-${thing.id}" class="remember__table__td__actions__dialog-box hidden">
-                <div class="disable-screen" onclick="togglePopup('dialog-box__confirm-delete-${thing.id}');">
-                    
+                <div id="dialog-box__confirm-delete-${thing.id}" class="remember__table__td__actions__dialog-box hidden">
+                    <div
+                      class="disable-screen"
+                      onclick="togglePopup('dialog-box__confirm-delete-${thing.id}');">
+            
+                    </div>
+                    <div class="dialog">
+                        <p>
+                            Please confirm.
+                        </p>
+                        <input
+                          id="confirm-delete-${thing.id}"
+                          type="button"
+                          value="Delete"
+                          onclick="removeRememberedThing('${thing.id}');"
+                        />
+                    </div>
                 </div>
-                <div class="dialog">
-                    <p>
-                        Please confirm.
-                    </p>
-                    <input
-                        id="confirm-delete-${thing.id}"
-                        type="button"
-                        value="Delete"
-                        onclick="removeRememberedThing('${thing.id}');"
-                    />
-                </div>
-            </div>
-        </span>
+            </span>
         </div>
-    `;
-
+        `;
+        
     document.getElementById( "item-" + thing.id ).innerHTML = tableRow;
+    
+}
+
+
+/**
+ * 
+ * @param {*} statusNumber 
+ * @param {*} statusToMatch 
+ */
+function selectStatus( statusNumber, statusToMatch ) {
+
+    return statusNumber == statusToMatch ? "selected" : "";
 
 }
 
-    // class="remember__table__tr" id="item-{id}"></div>
 
 /**
  * 
  */
-function removeTableRow() {
+function removeTableRow( thingID ) {
 
-}
-
-/**
- * 
- */
-function removeAllTableRows() {
+    let thingElement = document.getElementById( "item-" + thingID );
+    thingElement.parentNode.removeChild( thingElement );
 
 }
 
@@ -330,7 +391,6 @@ function filterTableBy() {
 function saveLocalStorage( newThing ) {
     let rememberedThings = getRememberedThings();
 
-    console.log( rememberedThings );
     rememberedThings[newThing.id] = newThing;
     
     window.localStorage.setItem( "keyIndex", newThing.id );
@@ -566,7 +626,7 @@ function validateSubcategory() {
 /**
  * 
  */
-function validateStatusPriority() {
+function validateStatus() {
     return true; // TODO
 }
 
@@ -598,7 +658,7 @@ function validateForm() {
         validateParentID() === true &&
         validateCategory() === true &&
         validateSubcategory() === true &&
-        validateStatusPriority() === true &&
+        validateStatus() === true &&
         validateFavorite() === true
     ) {
         return true;
@@ -609,7 +669,7 @@ function validateForm() {
 }
 
 
-function StatusPriorityToLanguage() {
+function StatusToLanguage() {
 
 }
 
@@ -625,7 +685,7 @@ function compileFormToThing() {
             "parentID": getInput( "parent-id" ),
             "category": getInput( "category" ),
             "subcategory": getInput( "subcategory" ),
-            "statusPriority": getInput( "status-priority" ),
+            "status": getInput( "status" ),
             "favorite": getInput( "favorite" ),
             "dateCreated": datetimeString(),
             "dateModified": "0000-00-00 00:00:00"
